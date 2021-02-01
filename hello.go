017f03095917477811b27565cd5c7d7f200c620b
Daniel Kurniadi <kurniadi.daniel@bytedance.com>
2021-02-01 21:47:38 +0800

    Hello: changes for v1.0.0

--- hello.go
+++ hello.go
@@ -4,10 +4,12 @@ import (
 	"rsc.io/quote/v3"
 )
 
+// Hello gives some greeting string
 func Hello() string {
 	return quote.HelloV3()
 }
 
+// Proverb gives some wisdomful string
 func Proverb() string {
 	return quote.Concurrency()
 }
